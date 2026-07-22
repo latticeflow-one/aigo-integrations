b392dbf4ea74d9e4ab8756024445d16683e214dd
Pavol Bielik <pbielik@acm.org>
2026-07-22 14:30:55 +0200

    feat: Add example for sample dependent run_inference (PE-443) (#78)
    Showcase the ability to define run_inference as:
    
    ```
    def run_inference(body: str, environment: dict[str, Any], sample: dict[str, Any] | None = None) -> str:
       ...
    ```
    
    which allows inference to access data from the sample. This is useful
    for sample-dependent initialization or interaction logic.
    
    ---------
    
    Co-authored-by: Pavol Bielik <pavol@latticeflow.ai>

---
+++ guides/model-sample-dependent-inference/models/run_inference.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+import json
+from typing import Any
+
+
+def run_inference(
+    body: str, environment: dict[str, Any], sample: dict[str, Any] | None = None
+) -> str:
+    # body_dict = json.loads(body)
+
+    # Note: Normally we would call the model endpoint here.
+    # response = httpx.post(
+    #     ...
+    # )
+
+    # For demonstration purposes, we will return the sample text.
+    response = sample["text"]
+
+    return json.dumps(
+        {"choices": [{"message": {"role": "assistant", "content": response}}]}
+    )

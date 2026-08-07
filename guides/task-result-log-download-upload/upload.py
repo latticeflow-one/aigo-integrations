49885f80b1e05bf0b9d18ddfb765d2c397b45e5c
Stefan <stefan@latticeflow.ai>
2026-08-07 16:55:12 +0300

    * update python sdk functions to reflect latest changes

--- guides/task-result-log-download-upload/upload.py
+++ guides/task-result-log-download-upload/upload.py
@@ -13,7 +13,7 @@ run_config = SDKRunConfig.model_validate(yaml.safe_load(Path("run.yaml").read_te
 # Create the same evaluation on the destination app.
 client = Client(ai_app_key="destination-app")
 print(f"[{client.ai_app_key}] Creating evaluation...")
-evaluation = client.create_evaluation(run_config.evaluation)
+evaluation = client.create_evaluation_run(run_config.evaluation)
 
 # Upload the saved log to the new task result.
 print(f"[{client.ai_app_key}] Uploading task result log from {LOG_PATH}...")
@@ -22,5 +22,5 @@ print(f"[{client.ai_app_key}] Task result log uploaded.")
 
 # Start the evaluation which leverages the uploaded task result log.
 print(f"[{client.ai_app_key}] Starting evaluation {evaluation.id}...")
-client.start_evaluation(evaluation.id, wait=True, poll_interval=5)
+client.start_evaluation_run(evaluation.id, wait=True, poll_interval=5)
 print(f"[{client.ai_app_key}] Evaluation complete.")
